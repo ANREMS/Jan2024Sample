0ce2daa4a9770903f637589a022457991a613528
Annadurai <choannadurai@gmail.com>
2024-03-15 22:33:42 +0000

    Initial commit

---
+++ src/test/java/Mycontactform.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Mycontactform {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://mycontactform.com/");
+        driver.findElement(By.linkText("Sample Forms")).click();
+
+    }
+}
